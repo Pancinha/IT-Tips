39a4e024343df6be5dbd6e7cbfba8c4e5a9afa15
Rafael Pancinha <54646423+Pancinha@users.noreply.github.com>
2022-02-24 10:59:18 -0300

    Commit init

---
+++ Libraries/javascript.js
@@ -0,0 +1,535 @@
+$(document).ready(function(){  //DESCE IMPRESSORAS
+    $(".in").click(function(){
+        $(".im").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".in").click(function(){ //SOBE CONTEUDO IMPRESSORAS
+        $(".conectImp").slideUp("slow");
+    });
+});
+
+$(document).ready(function(){  //DESCE IMPRESSORAS
+    $(".in").mouseenter(function(){
+        $(".im,.documentos").slideDown("slow");
+    });
+});
+
+$(document).ready(function(){  //DESCE IMPRESSORAS
+    $(".system").mouseenter(function(){
+        $(".localPacient,.solicitaCompra,.aberturaOrdem,.cadMat,.cadMed,.entUnic,.agnCon,.agenEx,.gestAgenCir,.documentos").slideDown("slow");
+    });
+});
+
+
+
+
+
+
+
+
+$(document).ready(function(){  
+    $(".im").mouseenter(function(){
+        $(".im").css("background-color", "#023158");
+    });
+});
+$(document).ready(function(){  
+    $(".im").mouseenter(function(){
+        $(".im").css("color", "white");
+    });
+});
+
+$(document).ready(function(){  
+    $(".im").mouseout(function(){
+        $(".im").css("background-color", "white");
+    });
+});
+$(document).ready(function(){  
+    $(".im").mouseout(function(){
+        $(".im").css("color", "black");
+    });
+});
+
+
+
+$(document).ready(function(){  
+    $(".localPacient").mouseenter(function(){
+        $(".localPacient").css("background-color", "#023158");
+    });
+});
+$(document).ready(function(){  
+    $(".localPacient").mouseenter(function(){
+        $(".localPacient").css("color", "white");
+    });
+});
+
+$(document).ready(function(){  
+    $(".localPacient").mouseout(function(){
+        $(".localPacient").css("background-color", "white");
+    });
+});
+$(document).ready(function(){  
+    $(".localPacient").mouseout(function(){
+        $(".localPacient").css("color", "black");
+    });
+});
+
+
+
+
+
+
+$(document).ready(function(){  
+    $(".solicitaCompra").mouseenter(function(){
+        $(".solicitaCompra").css("background-color", "#023158");
+    });
+});
+$(document).ready(function(){  
+    $(".solicitaCompra").mouseenter(function(){
+        $(".solicitaCompra").css("color", "white");
+    });
+});
+
+$(document).ready(function(){  
+    $(".solicitaCompra").mouseout(function(){
+        $(".solicitaCompra").css("background-color", "white");
+    });
+});
+$(document).ready(function(){  
+    $(".solicitaCompra").mouseout(function(){
+        $(".solicitaCompra").css("color", "black");
+    });
+});
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){  
+    $(".aberturaOrdem").mouseenter(function(){
+        $(".aberturaOrdem").css("background-color", "#023158");
+    });
+});
+$(document).ready(function(){  
+    $(".aberturaOrdem").mouseenter(function(){
+        $(".aberturaOrdem").css("color", "white");
+    });
+});
+
+$(document).ready(function(){  
+    $(".aberturaOrdem").mouseout(function(){
+        $(".aberturaOrdem").css("background-color", "white");
+    });
+});
+$(document).ready(function(){  
+    $(".aberturaOrdem").mouseout(function(){
+        $(".aberturaOrdem").css("color", "black");
+    });
+});
+
+
+
+
+
+
+$(document).ready(function(){  
+    $(".cadMat").mouseenter(function(){
+        $(".cadMat").css("background-color", "#023158");
+    });
+});
+$(document).ready(function(){  
+    $(".cadMat").mouseenter(function(){
+        $(".cadMat").css("color", "white");
+    });
+});
+
+$(document).ready(function(){  
+    $(".cadMat").mouseout(function(){
+        $(".cadMat").css("background-color", "white");
+    });
+});
+$(document).ready(function(){  
+    $(".cadMat").mouseout(function(){
+        $(".cadMat").css("color", "black");
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){  
+    $(".cadMed").mouseenter(function(){
+        $(".cadMed").css("background-color", "#023158");
+    });
+});
+$(document).ready(function(){  
+    $(".cadMed").mouseenter(function(){
+        $(".cadMed").css("color", "white");
+    });
+});
+
+$(document).ready(function(){  
+    $(".cadMed").mouseout(function(){
+        $(".cadMed").css("background-color", "white");
+    });
+});
+$(document).ready(function(){  
+    $(".cadMed").mouseout(function(){
+        $(".cadMed").css("color", "black");
+    });
+});
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){  
+    $(".entUnic").mouseenter(function(){
+        $(".entUnic").css("background-color", "#023158");
+    });
+});
+$(document).ready(function(){  
+    $(".entUnic").mouseenter(function(){
+        $(".entUnic").css("color", "white");
+    });
+});
+
+$(document).ready(function(){  
+    $(".entUnic").mouseout(function(){
+        $(".entUnic").css("background-color", "white");
+    });
+});
+$(document).ready(function(){  
+    $(".entUnic").mouseout(function(){
+        $(".entUnic").css("color", "black");
+    });
+});
+
+
+
+
+
+
+
+$(document).ready(function(){  
+    $(".agnCon").mouseenter(function(){
+        $(".agnCon").css("background-color", "#023158");
+    });
+});
+$(document).ready(function(){  
+    $(".agnCon").mouseenter(function(){
+        $(".agnCon").css("color", "white");
+    });
+});
+
+$(document).ready(function(){  
+    $(".agnCon").mouseout(function(){
+        $(".agnCon").css("background-color", "white");
+    });
+});
+$(document).ready(function(){  
+    $(".agnCon").mouseout(function(){
+        $(".agnCon").css("color", "black");
+    });
+});
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){  
+    $(".agenEx").mouseenter(function(){
+        $(".agenEx").css("background-color", "#023158");
+    });
+});
+$(document).ready(function(){  
+    $(".agenEx").mouseenter(function(){
+        $(".agenEx").css("color", "white");
+    });
+});
+
+$(document).ready(function(){  
+    $(".agenEx").mouseout(function(){
+        $(".agenEx").css("background-color", "white");
+    });
+});
+$(document).ready(function(){  
+    $(".agenEx").mouseout(function(){
+        $(".agenEx").css("color", "black");
+    });
+});
+
+
+
+
+
+
+
+
+$(document).ready(function(){  
+    $(".gestAgenCir").mouseenter(function(){
+        $(".gestAgenCir").css("background-color", "#023158");
+    });
+});
+$(document).ready(function(){  
+    $(".gestAgenCir").mouseenter(function(){
+        $(".gestAgenCir").css("color", "white");
+    });
+});
+
+$(document).ready(function(){  
+    $(".gestAgenCir").mouseout(function(){
+        $(".gestAgenCir").css("background-color", "white");
+    });
+});
+$(document).ready(function(){  
+    $(".gestAgenCir").mouseout(function(){
+        $(".gestAgenCir").css("color", "black");
+    });
+});
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".localPacient").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".localPacExp").slideUp("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".solicitaCompra").click(function(){
+        $(".solicitaCompraExp").slideToggle("slow");
+    });
+});
+
+
+
+
+
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".aberturaOrdem").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".aberturaOrdemExp").slideUp("slow");
+    });
+});
+
+
+
+
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".solicitaCompra").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".aberturaOrdemExp").slideUp("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".im").click(function(){
+        $(".conectImp").slideToggle("slow");
+    });
+});
+
+
+
+
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".cadMat").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".cadMatExp").slideUp("slow");
+    });
+});
+
+
+
+
+
+
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".cadMed").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".cadMedExp").slideUp("slow");
+    });
+});
+
+
+
+
+
+
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".entUnic").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".entUnicExp").slideUp("slow");
+    });
+});
+
+
+
+
+
+
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".agnCon").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".agnConExp").slideUp("slow");
+    });
+});
+
+
+
+
+
+
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".agenEx").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".agenExExp").slideUp("slow");
+    });
+});
+
+
+
+
+
+
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".gestAgenCir").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".gestAgenCirExp").slideUp("slow");
+    });
+});
+
+
+
+$(document).ready(function(){
+    $(".in, .system").click(function(){
+        $(".documentos").slideDown("slow");
+    });
+});
+
+
+
+
+
+
+////////////////
+$(document).ready(function(){
+    $(".localPacient").click(function(){
+        $(".localPacExp").slideToggle("slow");
+    });
+});
+
+
+$(document).ready(function(){
+    $(".aberturaOrdem").click(function(){
+        $(".imn2,.aberturaOrdemExp").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".cadMat").click(function(){
+        $(".cadMatExp").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".cadMed").click(function(){
+        $(".cadMedExp").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".entUnic").click(function(){
+        $(".entUnicExp").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".agnCon").click(function(){
+        $(".agnConExp").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".agenEx").click(function(){
+        $(".agenExExp").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".gestAgenCir").click(function(){
+        $(".gestAgenCirExp").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".system").click(function(){
+        $(".localPacExp,.aberturaOrdemExp,.cadMatExp,.cadMedExp,.entUnicExp,.agnConExp,.agenExExp,.gestAgenCirExp,.imn2,.solicitaCompraExp").slideUp("slow");
+    });
+});
+
+
+
+
+
